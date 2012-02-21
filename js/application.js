71025d0407930733e37beab89d934f2384b59426
Luke Barnett <l@en.vc>
2012-02-22 11:14:33 +1300

    Fixed detecting passed username correctly

--- js/application.js
+++ js/application.js
@@ -158,7 +158,7 @@ $(document).ready(function() {
 		})();
 		
 		var urlPassedUsername = urlParams["traktusername"];
-		if(urlPassedUsername != "")
+		if(urlPassedUsername != undefined)
 		{
 			$("#traktusername").val(urlPassedUsername);
 			checkLogin();
