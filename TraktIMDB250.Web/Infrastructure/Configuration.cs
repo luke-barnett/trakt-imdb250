e5b4cb68c9b2c4aa9d01070ad5b73df70493c61a
Luke Barnett <luke@barnett.net.nz>
2015-06-18 07:13:25 +1200

    Added in GA

--- TraktIMDB250.Web/Infrastructure/Configuration.cs
+++ TraktIMDB250.Web/Infrastructure/Configuration.cs
@@ -1,16 +1,11 @@
-﻿using System;
-using System.Configuration;
+﻿using System.Configuration;
 
 namespace TraktIMDB250.Web.Infrastructure
 {
 	public static class Configuration
 	{
-		public static string TraktClientId
-		{
-			get
-			{
-				return ConfigurationManager.AppSettings["TraktClientId"];
-			}
-		}
+		public static string TraktClientId { get { return ConfigurationManager.AppSettings["TraktClientId"]; } }
+
+		public static string GoogleAnalyticsTrackingCode { get { return ConfigurationManager.AppSettings["GoogleAnalyticsTrackingCode"]; } }
 	}
 }
